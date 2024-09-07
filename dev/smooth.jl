d5167edd37fa4befaba52d8c60e3322b0a7942bc
Documenter.jl <documenter@juliadocs.github.io>
2024-09-07 08:05:29 +0000

    build based on 8a4276e

--- dev/smooth.jl
+++ dev/smooth.jl
@@ -1,6 +1,8 @@
-function flexible_smooth_indicator(x, intervals, ε; mode = "bounded_intervals")
+function flexible_smooth_indicator(x, intervals, ε; mode="bounded_intervals")
     if mode == "bounded_intervals"
-        return sum(smooth_indicator_tanh.(x, interval[1], interval[2], ε) for interval in intervals)
+        return sum(
+            smooth_indicator_tanh.(x, interval[1], interval[2], ε) for interval in intervals
+        )
     elseif mode == "unbounded_intervals"
         return 0.5 * (tanh((x - intervals[1][1]) / ε) + 1)
     end
@@ -11,12 +13,12 @@ function smooth_indicator_tanh(x, a, b, ε)
 end
 
 # For bounded intervals
-fNC_bounded = (x, I, ϵ) -> flexible_smooth_indicator(x, I, ϵ, mode = "bounded_intervals")
+fNC_bounded = (x, I, ϵ) -> flexible_smooth_indicator(x, I, ϵ; mode="bounded_intervals")
 
 # For [a, +∞)
 fNC_unboundedplus =
-    (x, a, ϵ) -> flexible_smooth_indicator(x, [(a, 0)], ϵ, mode = "unbounded_intervals")
+    (x, a, ϵ) -> flexible_smooth_indicator(x, [(a, 0)], ϵ; mode="unbounded_intervals")
 
 # For (-∞,a]
 fNC_unboundedminus =
-    (x, a, ϵ) -> 1 - flexible_smooth_indicator(x, [(a, 0)], ϵ, mode = "unbounded_intervals")
+    (x, a, ϵ) -> 1 - flexible_smooth_indicator(x, [(a, 0)], ϵ; mode="unbounded_intervals")
